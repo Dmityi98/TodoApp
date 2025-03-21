131a85432214d21be71bcbf3337c353d1ed39a06
Dmityi98 <117891330+Dmityi98@users.noreply.github.com>
2025-03-21 09:50:26 +0300

    Update Program.cs

--- TodoApp/TodoApp/Program.cs
+++ TodoApp/TodoApp/Program.cs
@@ -23,6 +23,12 @@ if (app.Environment.IsDevelopment())
 
 app.MapControllers();
 
+app.UseCors(x =>
+{
+    x.WithHeaders().AllowAnyHeader();
+    x.WithOrigins("http://localhost:3000");
+    x.WithMethods().AllowAnyMethod();
+});
 
 
 app.Run();
